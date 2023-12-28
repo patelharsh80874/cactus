c57847a2d616204dece5926da5e6b7a021971af6
HARSH PATEL <110234600+patelharsh80874@users.noreply.github.com>
2023-12-28 17:44:50 +0530

    Delete cactus directory

--- cactus/scriptcactus.js
+++
@@ -1,6 +0,0 @@
-// var crsr = document.querySelector("#crsr");
-// document.querySelector("#main").addEventListener("mousemove", function (dets) {
-//     crsr.style.left = dets.clientX + "px";
-//     crsr.style.top = dets.clientY + "px";
-    
-// });
