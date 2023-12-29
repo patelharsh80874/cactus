68bfb8dac54d935a254a9a48a3bc06d7a4175b47
HARSH PATEL <110234600+patelharsh80874@users.noreply.github.com>
2023-12-29 12:54:20 +0530

    Update scriptcactus.js

--- scriptcactus.js
+++ scriptcactus.js
@@ -23,11 +23,34 @@ document.querySelector("#main").addEventListener("mousemove", function (dets) {
        duration:1,
        delay: 1.5
     })
+   gsap.from("#centertext",{
+        opacity:0,
+        duration:1,
+        delay:2.5,
+    })
     gsap.from("#leftcenter p",{
         opacity:0,
         x:-300,
        duration:1,
        delay: 1.5
+    })
+   gsap.from("#leftbot",{
+        y:200,
+        opacity:0,
+        duration:0.5,
+        delay:2
+    })
+    gsap.from("#rightcenter",{
+        x:200,
+        opacity:0,
+        duration:0.5,
+        delay:2
+    })
+    gsap.from("#rightbot",{
+        y:200,
+        opacity:0,
+        duration:0.5,
+        delay:2.5
     })
 
 
